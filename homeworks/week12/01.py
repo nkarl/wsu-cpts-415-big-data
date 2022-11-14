dc172fa31f841038111e19fd9a5f0e89ce75e415
nkarl <21993921+nkarl@users.noreply.github.com>
2022-11-14 13:59:28 -0800

    vault backup: 2022-11-14 13:59:28
    Affected files:
    homeworks/week12/01.py
    homeworks/week12/CPTS_415_NGUYEN_CHARLES.md

--- homeworks/week12/01.py
+++ homeworks/week12/01.py
@@ -5,31 +5,31 @@ class Person:
 
 class User:
     def __init__(self, person: Person, friends: dict[str, Person]):
-        self.person: Person             = person
-        self.friends: dict[str, Person] = friends
-        self.friends_count: int         = len(self.friends)
+        self.person: Person                = person
+        self.friendship: dict[str, Person] = friends
+        self.friends_count: int            = len(self.friendship)
 
     def __repr__(self):
-        return f"({self.person.name}, {[name for name in self.friends]})"
+        return f"({self.person.name}, {[name for name in self.friendship]})"
 
 
 def Map(i: User, j: User):
     if (i.friends_count > j.friends_count):
-        return (j, i.friends)
+        return (j, i.friendship)
     else:
-        return (i, j.friends)
+        return (i, j.friendship)
 
 
 
-def Reduce(user: User, other_friends: dict[str, Person]):
-    if len(other_friends) == 0 or len(user.friends) == 0:
+def Reduce(a: User, b_friendship: dict[str, Person]):
+    if len(a.friendship) == 0 or len(b_friendship) == 0:
         return {}
     else: 
-        potential: dict[str, Person] = dict()
-        for person in user.friends:
-            if person in other_friends:
-                potential[person] = user.friends[person]
-        return potential
+        common: dict[str, Person] = dict()
+        for person in a.friendship:
+            if person in b_friendship:
+                common[person] = a.friendship[person]
+        return common
 
 
 def MapReduce(i: User, j: User):
